84996a17d6bd536bec3048baba22aa37d515cb42
RTHag <57712495+RTHag@users.noreply.github.com>
2019-11-14 14:49:38 +0100

    contact dinges af

--- contact.php
+++ contact.php
@@ -16,13 +16,15 @@ include_once 'templates/navigation.php';
   <body>
 
     <div class="contactinfo">
-      <p>Email: tyfus.aids@cholera.yeet</p>
-      <p>Telefoonnummer: 911-112-09112001</p>
-      <p>Adres: Auschwitz-Birkenau Poland</p>
+      <p> <span class="info">Email: </span>tyfus.aids@cholera.yeet</p>
+      <p><span>Telefoonnummer: </span>911-112-09112001</p>
+      <p><span>Adres: </span>Auschwitz-Birkenau Poland</p>
     </div>
 
     <div class="mapslocation">
-
+      <p>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d41003.71977410538!2d19.22323822974701!3d50.035108331447226!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471694e04f384c31%3A0x1e1c6b2131976ebd!2zT8Wbd2nEmWNpbSwgUG9sZW4!5e0!3m2!1snl!2snl!4v1573738919847!5m2!1snl!2snl" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+      </p>
     </div>
 
   </body>

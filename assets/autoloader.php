7dc6995e7bfb2d19b5ad3ba23b6d4d9a87e4720d
dekstiertje <57712393+dekstiertje@users.noreply.github.com>
2019-11-14 10:37:10 +0100

    Update autoloader.php
    connectie met database wideworldimporters gemaakt.

--- assets/autoloader.php
+++ assets/autoloader.php
@@ -6,6 +6,22 @@
  * Date: 2019-11-07
  * Time: 19:38
  */
+
+
+ $servername = "localhost";
+ $username = "root";
+ $password = "";
+ $dbname = "wideworldimporters";
+
+ // Create connection
+ $conn = new mysqli($servername, $username, $password, $dbname);
+ // Check connection
+ if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+ }
+
+
+
 ?>
 <head>
     <title>KBS</title>

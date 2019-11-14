dffc54cf1260a96649533edc4f086a7e42a89f56
semdeboer <57707449+semdeboer@users.noreply.github.com>
2019-11-14 16:44:24 +0100

    Update product-detail.php

--- templates/product-detail.php
+++ templates/product-detail.php
@@ -6,6 +6,9 @@
     <div class="productname">
         <H1>naam product</H1>
     </div>
+    <div class="price">
+        <H1> €199</H1>
+    </div>
     <div class="picture"
          <H1></H1>
     <img src="https://cdn.babymarkt.com/babymarkt/img/107440/900/steiff-teddybeer-finn-40-cm-beige-a021097.jpg">
@@ -13,6 +16,11 @@
     <div class="description">
         <H1>omschrijving</H1>
     </div>
+    <div class="buy">
+
+        <input type="submit" value="BUY NOW" class="buy">
+    </div>
+
 </div>
 
 

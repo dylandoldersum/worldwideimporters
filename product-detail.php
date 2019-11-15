2276e2583faf47101a680d9459f64d1180751c5e
dylandoldersum <dylandoldersum@outlook.com>
2019-11-15 11:58:39 +0100

    Fixing layout a bit

--- product-detail.php
+++ product-detail.php
@@ -13,7 +13,6 @@ include_once 'templates/navigation.php';
 
 
 $sql= "SELECT StockItemName, RecommendedRetailPrice, TypicalWeightPerUnit From stockitems WHERE StockItemID=". $_GET['itemID'];
-
 $result = mysqli_query($conn, $sql);
 
 foreach ($result as $value) {
@@ -25,26 +24,28 @@ foreach ($result as $value) {
 
 ?>
 <div class="product-container">
-    <div class="productname">
-        <H1><?php print $itemName ?></H1>
+    <div class="first-row">
+        <div class="productname">
+            <H1><?php print $itemName ?></H1>
+        </div>
+        <div class="price">
+            <H1> € <?php print $itemPrice ?></H1>
+        </div>
     </div>
-    <div class="price">
-        <H1> € <?php print $itemPrice ?></H1>
+    <div class="mid-row">
+        <div class="picture">
+             <H1></H1>
+        <img src="https://cdn.babymarkt.com/babymarkt/img/107440/900/steiff-teddybeer-finn-40-cm-beige-a021097.jpg">
+        </div>
+        <div class="description">
+            <H1>omschrijving</H1> <br>
+            <H2>gewicht</H2> <br>
+            <H3><?php print $itemWeight ?> KG</H3>
+            <div class="buybutton">
+                <input type="submit" value="IN WINKELWAGEN" class="buy">
+            </div>
+        </div>
     </div>
-    <div class="picture"
-         <H1></H1>
-    <img src="https://cdn.babymarkt.com/babymarkt/img/107440/900/steiff-teddybeer-finn-40-cm-beige-a021097.jpg">
-    </div>
-    <div class="description">
-        <H1>omschrijving</H1> <br>
-        <H2>gewicht</H2> <br>
-        <H3><?php print $itemWeight ?> KG</H3>
-    </div>
-    <div class="buybutton">
-
-        <input type="submit" value="IN WINKELWAGEN" class="buy">
-    </div>
-
 </div>
 </body>
 

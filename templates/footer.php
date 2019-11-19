fbadee0a96a9503a6332790531fec5a15a379e1e
semdeboer <57707449+semdeboer@users.noreply.github.com>
2019-11-19 10:51:28 +0100

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -13,13 +13,7 @@
       <a href="contact.php">Contact</a>
       <div class="footerlogo">
           <img src="assets/images/logo.png">
-          <div class="locationlogo">
-              <img src="https://cdn3.iconfinder.com/data/icons/google-material-design-icons/48/ic_location_on_48px-512.png">
-              <div class="logo">
-              <p>21, bokjes straat</p>
-              <p>Urk, Netherlands</p>
-              </div>
-          </div>
+
       </div>
 
 

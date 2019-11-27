634cd1db3467603f97485bb65d821447dc202fd3
RTHag <57712495+RTHag@users.noreply.github.com>
2019-11-27 12:32:22 +0100

    winkelwagen

---
+++ winkelwagen.php
@@ -0,0 +1,31 @@
+<?php
+/** Automatisch laden van header */
+include_once 'assets/autoloader.php';
+
+/** Templates met gebruik van includes **/
+include_once 'templates/navigation.php';
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="assets/css/stylesheet.css">
+  </head>
+  <body>
+
+    <div class="container_winkelwagen">
+      <div class="header_winkelwagen">
+        <p>Winkelwagen</p>
+      </div>
+      <div class="product_winkelwagen">
+        
+      </div>
+    </div>
+
+    <?php
+      include_once 'templates/footer.php';
+     ?>
+  </body>
+</html>

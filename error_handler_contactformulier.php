4a1fa855af1f0297c6a898274eadbc1e635c0eb5
semdeboer <57707449+semdeboer@users.noreply.github.com>
2019-12-02 13:19:27 +0100

    fch

--- error_handler_contactformulier.php
+++ error_handler_contactformulier.php
@@ -4,20 +4,21 @@
     $first = $_POST["first"];
     $last = $_POST["last"];
     $email = $_POST["email"];
+    $bericht = $_POST["bericht"];
 
     //Kijkt of de velden leeg zijn
-    if (empty($first) || empty($last) || empty($email)) {
-      header("Location: contact.php?signup=empty");
+    if (empty($first) || empty($last) || empty($email) || empty($bericht)) {
+      header("Location: contact.php?signup=empty&first=$first&last=$last&email=$email");
       exit();
     } else {
       //Kijkt of de namen wel valide karakters bevatten
       if (!preg_match("/^[a-zA-Z]*$/", $first) AND !preg_match(" ", $last)) {
-        header("Location: contact.php?signup=char");
+        header("Location: contact.php?signup=char&email=$email");
         exit();
       } else {
         //Kijkt of de invoer voor de email wel goed is
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-          header("Location: contact.php?signup=email");
+          header("Location: contact.php?signup=email&first=$first&last=$last");
           exit();
         } else {
           header("Location: contact.php?signup=success");

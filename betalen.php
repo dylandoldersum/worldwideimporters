b4d63c1dbd7c5dd93b44d13d4729ccb37c802524
dylandoldersum <dylandoldersum@outlook.com>
2019-12-04 18:36:09 +0100

    bestellijn in mollie

--- betalen.php
+++ betalen.php
@@ -20,8 +20,16 @@ $email = $_POST["email"];
 $array_info = array("Voornaam" => $first, "Achternaam" => $last, "Adres" => $address, "Huisnummer" => $number, "Landnaam" => $country, "Postcode" => $postalcode, "Telefoonnummer" => $phone, "Emailadres" => $email);
 $_SESSION['contactinfo'] = $array_info;
 
+$bestelline = "";
+
+foreach ($_SESSION['itemID'] as $arrayitem) {
+    $bestelline .= $arrayitem['quantity'] . " X " . $arrayitem['pname'] . " = " . $arrayitem['quantity'] * $arrayitem['price'] . "  ";
+}
+
 $desc = $_SESSION['contactinfo']['Voornaam'];
-$desc .= " ".$_SESSION['contactinfo']['Achternaam']." ".rand(10000, 20000);
+$desc .= " ".$_SESSION['contactinfo']['Achternaam']." - ".rand(10000, 20000);
+$desc .= " - ";
+$desc .= "$bestelline";
 
 $json = generateMollie($desc, doubleval($_SESSION['TOT']));
 

6dda212daf9f0b0565e1d8aabd66be63246d0d86
dekstiertje <57712393+dekstiertje@users.noreply.github.com>
2019-12-19 13:52:44 +0100

    Update addcustomer.php

--- addcustomer.php
+++ addcustomer.php
@@ -3,8 +3,9 @@ include "assets/autoloader.php";
 
 if (!isset($_SESSION['loggedin'])) {
   createCustomerifnoAccount();
-  addOrder();
-  header('location: bedankt.php');
 }
+
+addOrder();
+header('location: bedankt.php');
 
  ?>

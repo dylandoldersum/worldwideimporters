df0701c7ffd8fe274d875645659a4c6f096bbb58
LuukBruggeman <bruggemanluuk@gmail.com>
2020-01-09 13:28:09 +0100

    Update herkansing functies.php

--- classes/herkansing functies.php
+++ classes/herkansing functies.php
@@ -10,8 +10,8 @@ function bestelgeschiedenis(){
     $connection = mysqli_connect($host, $user, $password, $dbName);
     $sql = "SELECT StockItemName, RecommendedRetailPrice FROM stockitems WHERE StockItemID IN 
             (SELECT StockItemID FROM orderline1 WHERE orderID IN 
-
-            (SELECT orderID FROM order1 WHERE CustomerID =".$_SESSION["CustomerID"];
+            (SELECT orderID FROM order1 WHERE CustomerID IN 
+            (SELECT CustomerID FROM accounts WHERE email=".$_SESSION["Emailadres"];
 
 
     $result = mysqli_query($connection, $sql);

d32d719e24dee2db223574e6e84814b74580bb70
LuukBruggeman <bruggemanluuk@gmail.com>
2020-01-09 10:29:03 +0100

    afd

---
+++ classes/herkansing functies.php
@@ -0,0 +1,11 @@
+<?php
+
+function bestelgeschiedenis(){
+    $host = 'localhost';
+    $dbName = 'wideworldimporters';
+    $user = 'root';
+    $password = '';
+    $connection = mysqli_connect($host, $user, $password, $dbName);
+    
+
+}
